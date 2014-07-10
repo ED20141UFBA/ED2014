7b2999e218c3fb8c9958b8dffd194c32a981ddb3
igorandrade83 <igorandrade@dcc.ufba.br>
2014-07-09 22:32:40 -0300

    Comentando...

--- funcoes.h
+++ funcoes.h
@@ -9,7 +9,7 @@
 
 typedef struct proc
 {
-	int ID;
+	int ID;				// Cada nó só terá 1 único valor de ID, será gerado pela incrementação do inteiro GeraID
 	char NomeCliente [50];
 	char EnderecoCliente [50];
 	char RGCliente [12];
@@ -17,22 +17,22 @@ typedef struct proc
 	float ValorCheque;
 	char DataCheque[11];
 	char NomeMercado [31];
-	struct proc *prox;
-} Processo;
+	struct proc *prox;		// Ponteiro para o próximo nó
+} Processo;	
 
 
 
-void Menu(Processo *Pilha);
-void Inicializar(Processo *Pilha);
-int teste_vazia(Processo *Pilha);
-void ExibirProcessos(Processo *Pilha);
-void libera(Processo *Pilha);
-void EmpilharProcesso(Processo *Pilha);
-void Apagar(Processo *Pilha);
-void OrganizarUltimoProcesso(Processo *Pilha);
-void OrganizaProcessoPorNomeMercado(Processo *Pilha);
-void ApagarID(Processo *Pilha);
-void ExibirID(Processo *Pilha);
-void ExibirProximoProcesso(Processo *Pilha);
-void ExibirUltimoProcesso(Processo *Pilha);
-void ExibeNo(Processo *No);
+void Menu(Processo *Pilha);				// Menu
+void Inicializar(Processo *Pilha);			// Inicializa a Pilha
+int teste_vazia(Processo *Pilha);			// Verifica se a pilha está vazia
+void ExibirProcessos(Processo *Pilha);			// Exibe todos os processos
+void libera(Processo *Pilha);				// Limpa a pilha
+void EmpilharProcesso(Processo *Pilha);			// Empilha
+void Apagar(Processo *Pilha);				// Desempilha
+void OrganizarUltimoProcesso(Processo *Pilha);		// A função EmpilharProcesso insere um processo sempre no topo da pilha, essa função remaneja para a posição correta.
+void OrganizaProcessoPorNomeMercado(Processo *Pilha);	// Ordena todos os processos com o nome pesquisado para o topo da pilha
+void ApagarID(Processo *Pilha);				// Apaga um processo
+void ExibirID(Processo *Pilha);				// Imprime o ID pesquisado
+void ExibirProximoProcesso(Processo *Pilha);		// Imprime o processo no topo.
+void ExibirUltimoProcesso(Processo *Pilha);		// Imprime somente o processo na base da pilha
+void ExibeNo(Processo *No);				// Imprime o processo passado.
